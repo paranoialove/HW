3c67109d5e602b95576ad8ec287da7b6b4781ed9
Paranoialove <akazuma82@gmail.com>
2016-01-18 17:00:53 +0800

    HW:身分證檢察 (未完)
    待改善(for迴圈)

--- IdVerification/src/IdVerification.java
+++ IdVerification/src/IdVerification.java
@@ -32,8 +32,6 @@ public class IdVerification {
         int cvn1 = Character.getNumericValue(cityVaule.charAt(0)); //十位數
         int cvn2 = Character.getNumericValue(cityVaule.charAt(1)); //個位數
 
-        int y;//加權總數(不含
-
         //char y1 = id.charAt(1);
         int y11 = Character.getNumericValue(id.charAt(1));
         int y22 = Character.getNumericValue(id.charAt(2));
@@ -43,16 +41,18 @@ public class IdVerification {
         int y66 = Character.getNumericValue(id.charAt(6));
         int y77 = Character.getNumericValue(id.charAt(7));
         int y88 = Character.getNumericValue(id.charAt(8));
-        int y99 = Character.getNumericValue(id.charAt(9));
 
-        //for (int i = 1; i < 9; i++) {
-        //    for (int j = 8; j > 0; j--) {
-        //        y = (Character.getNumericValue(id.charAt(i)) * j) + y;
-        //    }
+        //加權總數(不含英文代碼加權)
+        //int L = 0;
+        //for (int i = 1; i < 8; i++) {
+        //    L = L + Character.getNumericValue(id.charAt(i)) * (9 - i);
         //}
-        //System.out.println("y = " + y);
-        y = y11 * 8 + y22 * 7 + y33 * 6 + y44 * 5 + y55 * 4 + y66 * 3 + y77 * 2 + y88 * 1 + y99;
-        int Y = cvn1 + cvn2 * 9 + y;
+        int y = 0;
+        System.out.println("y = " + y);
+        int y99 = Character.getNumericValue(id.charAt(9));
+        y = y11 * 8 + y22 * 7 + y33 * 6 + y44 * 5 + y55 * 4 + y66 * 3 + y77 * 2 + y88 * 1;
+        int Y = cvn1 + cvn2 * 9 + y + y99;
+
         System.out.println("驗證數字為" + Y);
         int z = Y % 10;
         System.out.println("餘數為：" + z);
